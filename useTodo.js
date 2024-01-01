1016428dbc103c226a0ec8125a5567a34272be08
LeoPC <bergesio90@gmail.com>
2024-01-01 13:00:33 +0100

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/TodoReducer';
+
+const init = () => {
+	return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = () => {
+	const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
+	const handleNewTodo = (todo) => {
+		const action = {
+			type: '[TODO] Add Todo',
+			payload: todo,
+		};
+		dispatch(action);
+	};
+
+	const handleDeleteTodo = (id) => {
+		const action = {
+			type: '[TODO] Remove Todo',
+			payload: id,
+		};
+		dispatch(action);
+	};
+
+	const handleToggleTodo = (id) => {
+		const action = {
+			type: '[TODO] Toggle Todo',
+			payload: id,
+		};
+		dispatch(action);
+	};
+
+	return {
+		todos,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleToggleTodo,
+		pendingTodosCount: todos.filter((todo) => !todo.done).length,
+		todosCount: todos.length,
+	};
+};
